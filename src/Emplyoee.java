a14a783dcf1ba1a20183c98cab92e764c0bc4d68
Veera Kishon Kumar Mucherla <veerakishon.mucherla@gmail.com>
2018-05-30 14:11:06 -0400

    Added copy constructor and hashcode,equals methods.

--- src/Emplyoee.java
+++ src/Emplyoee.java
@@ -50,5 +50,45 @@ public class Emplyoee {
 		System.out.println("Name of he employee is : " + this.empNumber);
 		System.out.println("Emplyoee department : " + this.empDept);
 	}
+	// Copy constructor
+
+	public Emplyoee(Emplyoee otherEmployee) {
+		this.empNumber = otherEmployee.empNumber;
+		this.eName = otherEmployee.eName;
+		this.empType = otherEmployee.empType;
+		this.empDept = otherEmployee.empDept;
+		this.empPass = otherEmployee.empPass;
+	}
+
+	public Emplyoee(int empNumber, String eName, String empType, int empDept, String empPass) {
+		super();
+		this.empNumber = empNumber;
+		this.eName = eName;
+		this.empType = empType;
+		this.empDept = empDept;
+		this.empPass = empPass;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return this.getEmpNumber() == ((Emplyoee) obj).getEmpNumber();
+	}
 
 }
